6ba8af94d309ace0766c942adddc3e8ea408f805
PatoEngravatado <140520388+PatoEngravatado@users.noreply.github.com>
2024-10-18 12:20:41 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,11 @@
 criaCartao(
     'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
     'O que é Python?',
     'O Python é uma linguagem de programação'
 )
